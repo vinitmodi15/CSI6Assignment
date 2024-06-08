134179bdb346c971e4d110b4c4f917eb7c942b98
Vinit Modi <vinitjodhpur2@gmail.com>
2024-06-08 16:39:23 +0530

    updated

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+// console.log("Hello World");
+const http= require('http');
+const PORT = 3000;
+
+const server = http.createServer((req,res)=>{
+    res.write('Hello World!');
+    res.end();
+})
+
+server.listen(3000,()=>{
+    console.log('Server running on port'+ PORT);
+})
