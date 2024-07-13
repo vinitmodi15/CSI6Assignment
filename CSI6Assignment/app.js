69de41e42727e15877932e4e59a84bbe5e09c2e1
vinit modi <vinitjodhpur2@gmail.com>
2024-07-13 23:47:30 +0530

    CSI6

--- CSI6Assignment/app.js
+++ CSI6Assignment/app.js
@@ -1,18 +1,9 @@
-// app.js (main application file)
+// app.js
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
-const productController = require('./products.controller');
+const productsRouter = require('./routes/products');
 
-mongoose.connect('mongodb://localhost/products', { useNewUrlParser: true, useUnifiedTopology: true });
-
-app.use(express.json());
-
-app.post('/products', productController.createProduct);
-app.get('/products', productController.getProducts);
-app.get('/products/:id', productController.getProduct);
-app.put('/products/:id', productController.updateProduct);
-app.delete('/products/:id', productController.deleteProduct);
+app.use('/api/products', productsRouter);
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
---
+++ CSI6Assignment/routes/products.js
@@ -0,0 +1,12 @@
+// routes/products.js
+const express = require('express');
+const router = express.Router();
+const productController = require('../controllers/products.controller');
+
+router.post('/', productController.createProduct);
+router.get('/', productController.getProducts);
+router.get('/:id', productController.getProduct);
+router.put('/:id', productController.updateProduct);
+router.delete('/:id', productController.deleteProduct);
+
+module.exports = router;
